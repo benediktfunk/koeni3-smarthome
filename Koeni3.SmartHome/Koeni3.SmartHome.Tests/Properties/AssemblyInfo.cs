9e03e182f882156d5252aa9b45d4303207b53040
Benedikt Funk <benedikt.funk@googlemail.com>
2023-08-07 09:41:41 +0200

    build: init commit

---
+++ Koeni3.SmartHome/Koeni3.SmartHome.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Koeni3.SmartHome.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Haufe Group")]
+[assembly: AssemblyProduct("Koeni3.SmartHome.Tests")]
+[assembly: AssemblyCopyright("Copyright © Haufe Group 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a91acb38-83b4-4961-bf7e-18629695f55a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Koeni3.SmartHome/Koeni3.SmartHome.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Koeni3.SmartHome.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
